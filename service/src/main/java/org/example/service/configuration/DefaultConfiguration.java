9afa9f88a752b1e394fcca93e95f6d691209164d
atodonoghuesbd <79602685+atodonoghuesbd@users.noreply.github.com>
2021-04-20 16:25:03 -0400

    Updated some stuff Bernie asked for...

--- service/src/main/java/org/example/service/configuration/DefaultConfiguration.java
+++ service/src/main/java/org/example/service/configuration/DefaultConfiguration.java
@@ -3,6 +3,7 @@ package org.example.service.configuration;
 import org.example.api.GreetingService;
 import org.example.service.ConfigurableHelloService;
 import org.example.service.HelloMarsService;
+import org.example.service.HelloPlutoService;
 import org.example.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class DefaultConfiguration {
     private static final String MARS = "mars";
     private static final String JUPITER = "jupiter";
+    private static final String PLUTO = "pluto";
 
     @Value("${planet:earth}")
     public String planet;
@@ -25,6 +27,8 @@ public class DefaultConfiguration {
                 return new HelloMarsService();
             case JUPITER:
                 return new ConfigurableHelloService(JUPITER);
+            case PLUTO:
+                return new HelloPlutoService();
             default:
                 return new HelloWorldService();
         }
